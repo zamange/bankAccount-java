b88068daa8cea5ea46e62e1094d1f5797317e3f6
WTC <zngeyane023@student.wethinkcode.co.za>
2024-05-01 11:50:29 +0200

    testing the SavingsAccount class

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -10,5 +10,14 @@ public class SavingsAccount extends BankAccount{
 
     public void calculateInterest() {
         double interest = getBalance() * interestRate / 100;
+        deposit(interest);
+        System.out.println("Interest calculated and added to account " + getAccountNumber());
+    }
+    public static void main(String[] args) {
+        // Testing the SavingsAccount class
+        SavingsAccount savingsAccount = new SavingsAccount("1234567890", 1000.0, 5.0); // 5% interest rate
+        System.out.println("Initial Balance: " + savingsAccount.getBalance());
+        savingsAccount.calculateInterest();
+        System.out.println("Current Balance after interest: " + savingsAccount.getBalance());
     }
 }
