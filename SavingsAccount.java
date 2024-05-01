cb1d93bc5474c33b756f7d22f41da010b26dfdfd
WTC <zngeyane023@student.wethinkcode.co.za>
2024-05-01 11:46:52 +0200

    constructor

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,2 +1,14 @@
-public class SavingsAccount {
+public class SavingsAccount extends BankAccount{
+    private double interestRate;
+
+
+    //constructor
+    public SavingsAccount(String accountNumber, double initialBalance, double interestRate) {
+        super(accountNumber, initialBalance);
+        this.interestRate = interestRate;
+    }
+
+    public void calculateInterest() {
+        balance += interestRate;
+    }
 }
